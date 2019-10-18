38b57e04bed21d04f6cca3bbeb8a498c3824d589
Isabel Raposo <isabelraposo@users.noreply.github.com>
2019-10-18 23:56:48 +0100

    Encapsulate into functions the handling of the questions

--- chatbot.py
+++ chatbot.py
@@ -73,6 +73,46 @@ def get_all_documents_content(list_docs_object):
     '''
     return [ [document_title(doc_obj) , get_document_content(doc_obj)] for  doc_obj  in list_docs_object]
 
+def get_questions(xml_file_name):
+    '''
+    Importar perguntas para formato a ser processado
+    '''
+
+    ''' get the content of all the document in the xml structure '''
+    xml_docs = get_documents_xml_file(xml_file_name)
+    ''' get the list of all docs, where each doc have content associated [title, [answer_id,questions] ] '''
+    docs_parsed = get_all_documents_content(xml_docs)
+    ''' print the first 2 documents '''
+    #print(docs_parsed[:2])   
+
+    questions = []
+    for doc in docs_parsed:
+        titulo = preprocess_sentence(doc[0])
+        for faq in doc[1]:
+            # iterate over variations of same faq
+            for i, q in enumerate(faq[1]):
+                # preprocessing
+                prep = preprocess_sentence(q)
+                if prep != []:
+                    questions.append([prep,faq[0],titulo])
+
+    return questions
+
+
+
+def get_queries(test_file_name):
+    '''
+    Importar perguntas para formato a ser processado
+    '''
+    
+   test_file = open(test_file_name, mode="r", encoding="utf-8")
+
+   queries = []
+   for query in test_file:
+       prep = preprocess_sentence(query)
+       queries.append(prep)
+
+   return queries
 
 
 def preprocess_sentence(sent):
@@ -124,44 +164,31 @@ def get_ID(query, questions):
 
     return best_id
 
+def write_results(queries, questions):
+    '''
+    Get best ID for each query and write in results.txt
+    '''
+
+    results_file = open("results.txt", mode="w", encoding="utf-8")
+
+    for query in queries[:-1]:
+        results_file.write(get_ID(query, questions)+"\n")
+
+    results_file.write(get_ID(queries[-1], questions))
+
 
 def main():
 
    ''' get the xml file name and test file name from command line '''
-   xml_file_name,test_file_name  = sys.argv[1: ]
+   xml_file_name, test_file_name  = sys.argv[1: ]
 
-   test_file = open(test_file_name, mode="r", encoding="utf-8")
-   results_file = open("results.txt", mode="w", encoding="utf-8")
-
-   ''' get the content of all the document in the xml structure '''
-   xml_docs = get_documents_xml_file(xml_file_name)
-   ''' get the list of all docs, where each doc have content associated [title, [answer_id,questions] ] '''
-   docs_parsed = get_all_documents_content(xml_docs)
-   ''' print the first 2 documents '''
-   #print(docs_parsed[:2])   
-
-   queries = []
-   for query in test_file:
-       prep = preprocess_sentence(query)
-       queries.append(prep)
+   queries = get_queries(xml_file_name)
 
    # formato de elemento de questions: [pergunta, id, titulo]
-   questions = []
-   for doc in docs_parsed:
-       titulo = preprocess_sentence(doc[0])
-       for faq in doc[1]:
-           # iterate over variations of same faq
-           for i, q in enumerate(faq[1]):
-               # preprocessing
-               prep = preprocess_sentence(q)
-               if prep != []:
-                    questions.append([prep,faq[0],titulo])
+   questions = get_questions(test_file_name)
 
    # find best match and print in results.txt
-   for query in queries[:-1]:
-       results_file.write(get_ID(query, questions)+"\n")
-   results_file.write(get_ID(queries[-1], questions))
-
+   write_results(queries, questions)
 
 
 
