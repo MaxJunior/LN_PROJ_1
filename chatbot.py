d4764c5794972d3728b499c406025fd905bb5a1e
Isabel Raposo <isabelraposo@users.noreply.github.com>
2019-10-19 12:03:53 +0100

    corrected the errors with unidecode

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,9 @@
-import sys
+import sys, re
 from utils  import  ID, DOCUMENT, QUESTION, ANSWER,FAQ, TITLE
 from xml.dom import minidom
 from nltk.stem import RSLPStemmer
 import nltk.corpus as corpus
 from nltk import word_tokenize
-import unidecode
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
@@ -115,6 +114,48 @@ def get_queries(test_file_name):
    return queries
 
 
+
+def preproc_aux(l):
+    '''
+    Funcao retirada do Lab3, do ficheiro DIST.py
+     - elimina acentos
+     - tudo em minusculas
+     - elimina pontuacao
+     - fica so com as perguntas
+    '''
+    # ELIMINA ACENTOS
+    l = re.sub(u"ã", 'a', l)
+    l = re.sub(u"á", "a", l)
+    l = re.sub(u"à", "a", l)
+    l = re.sub(u"õ", "o", l)
+    l = re.sub(u"ô", "o", l)
+    l = re.sub(u"ó", "o", l)
+    l = re.sub(u"é", "e", l)
+    l = re.sub(u"ê", "e", l)
+    l = re.sub(u"í", "i", l)
+    l = re.sub(u"ú", "u", l)
+    l = re.sub(u"ç", "c", l)
+    l = re.sub(u"Ã", 'A', l)
+    l = re.sub(u"Á", "A", l)
+    l = re.sub(u"À", "A", l)
+    l = re.sub(u"Õ", "O", l)
+    l = re.sub(u"Ô", "O", l)
+    l = re.sub(u"Ô", "O", l)
+    l = re.sub(u"Ó", 'O', l)
+    l = re.sub(u"Í", "I", l)
+    l = re.sub(u"Ú", "U", l)
+    l = re.sub(u"Ç", "C", l)
+    l = re.sub(u"É", "E", l)
+    # TUDO EM MINÚSCULAS
+    l = l.lower()
+    # ELIMINA PONTUAÇÃO
+    l = re.sub("[?|\.|!|:|,|;]", '', l)
+    # fica so com as perguntas
+    l = re.sub("^\w+\t+[^\w]", '', l)
+    return l
+
+
+
 def preprocess_sentence(sent):
     '''
     Pre-processamento das perguntas:
@@ -125,7 +166,7 @@ def preprocess_sentence(sent):
     '''
 
     # remove accentuation
-    sent = unidecode.unidecode(sent)
+    sent = preproc_aux(sent)
 
     # tokenize
     tokens = word_tokenize(sent)
